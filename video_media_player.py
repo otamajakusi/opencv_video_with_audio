1c2a970c8c660951dbe5cbb5a4919de9eaf3fcdc
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2021-12-19 18:34:35 +0900

    support pyaudio

--- video_media_player.py
+++ video_media_player.py
@@ -1,22 +1,78 @@
 import cv2
 import time
+
+# for WithPyAudio
+import pyaudio
+import wave
+
+# for WithMediaPlayer
 from ffpyplayer.player import MediaPlayer
 
 
-def main(file):
-    cap = cv2.VideoCapture(file)
-    player = MediaPlayer(file)
+class WithPyAudio:
+    # https://people.csail.mit.edu/hubert/pyaudio/docs/#example-callback-mode-audio-i-o
+
+    def __init__(self, audio_file):
+        wf = wave.open(audio_file, "rb")
+        p = pyaudio.PyAudio()
+        stream = p.open(
+            format=p.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True,
+            stream_callback=self._stream_cb,
+        )
+        self.wf = wf
+        self.p = p
+        self.stream = stream
+
+    def get_frame(self):
+        if not self.stream.is_active():
+            return False
+        return True
+
+    def close(self):
+        self.stream.stop_stream()
+        self.stream.close
+        self.p.terminate()
+        self.wf.close()
+
+    def _stream_cb(self, in_data, frame_count, time_info, status):
+        data = self.wf.readframes(frame_count)
+        return (data, pyaudio.paContinue)
+
+
+class WithMediaPlayer:
+    def __init__(self, audio_file):
+        self.player = MediaPlayer(audio_file)
+
+    def get_frame(self):
+        _, val = self.player.get_frame(show=False)
+        if val == "eof":
+            return False
+        return True
+
+    def close(self):
+        if self.player:
+            self.player.close_player()
+            self.player = None
+
+
+def main(video_file, audio_file):
+    cap = cv2.VideoCapture(video_file)
+
+    # player = WithMediaPlayer(audio_file)
+    player = WithPyAudio(audio_file)
     start_time = time.time()
 
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
-        _, val = player.get_frame(show=False)
-        if val == 'eof':
+        if not player.get_frame():
             break
 
-        cv2.imshow(file, frame)
+        cv2.imshow(video_file, frame)
 
         elapsed = (time.time() - start_time) * 1000  # msec
         play_time = int(cap.get(cv2.CAP_PROP_POS_MSEC))
@@ -24,7 +80,7 @@ def main(file):
         if cv2.waitKey(sleep) & 0xFF == ord("q"):
             break
 
-    player.close_player()
+    player.close()
     cap.release()
     cv2.destroyAllWindows()
 
@@ -33,6 +89,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", required=True)
+    parser.add_argument("--video", required=True)
+    parser.add_argument("--audio")
     args = parser.parse_args()
-    main(args.input)
+    main(args.video, args.audio if args.audio else args.video)
